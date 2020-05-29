6314123414818423a723616f2ba89fc4d7739d1b
lucyyz <17840801+lucyyz@users.noreply.github.com>
2020-05-28 17:39:50 -0700

    contact format finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -24,7 +24,9 @@ import { ContactComponent } from './contact/contact.component';
   ],
   providers: [],
   bootstrap: [
-    AppComponent
-    ]
+    AppComponent,
+    NavbarComponent,
+    ContactComponent
+  ]
 })
 export class AppModule { }
